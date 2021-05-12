7e91c7167ee4e7a2ddbf362235c990a8d684cbc5
acastillo456 <acastillo@develoop.net>
2021-05-12 12:09:23 -0400

    Barcode

---
+++ custom_modules/product_packaging_custom/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Product Packaging Custom',
+    'category': 'Sales',
+    'version': '0.0',
+    'summary': """Permite asociar un empaquetado a un paquete con su ubicación.""",
+    'description': """""",
+    'depends': ['product',"stock"],
+    'data': [
+        'views/product_packaging.xml',
+    ],
+    'author': 'Develoop Software',
+    'images': ['static/description/icon.png'],
+    'maintainer': 'Develoop Software',
+    'website': 'https://www.develoop.net',
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
