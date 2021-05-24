65c89eb9c06d7873a11bb71fe215d6e91756e3a7
acastillo456 <acastillo@develoop.net>
2021-05-24 15:44:11 -0400

    Product Price List

---
+++ custom_modules/product_price_list/models/product_template.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
+class Product_Template_Price_Custom(models.Model):
+
+    _inherit = 'product.template'
+
+    x_list_price_sale = fields.One2many('product.pricelist.item', string='Márgenes x tarifa', compute="_compute_x_list_price_sale")
+
+    def _compute_x_list_price_sale(self):
+        for record in self:
+            product_pricelist_items = self.env['product.pricelist.item'].search([('product_tmpl_id','=',record.id)])
+            if product_pricelist_items and len(product_pricelist_items):
+                for data in product_pricelist_items:
+                    data.x_price_cost_difference = (data.fixed_price - record.standard_price)
+                record.x_list_price_sale = product_pricelist_items
+            else:
+                record.x_list_price_sale = False
+
+class PricelistItem_custom(models.Model):
+
+    _inherit = "product.pricelist.item"
+
+    x_price_cost_difference = fields.Float(string="Margen")
