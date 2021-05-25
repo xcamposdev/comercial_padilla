e7c6dae36498e31eef413d0f3852c3be2a202f32
acastillo456 <acastillo@develoop.net>
2021-05-25 11:12:24 -0400

    Listado de precio en productos

--- custom_modules/product_price_list/models/product_template.py
+++ custom_modules/product_price_list/models/product_template.py
@@ -15,7 +15,12 @@ class Product_Template_Price_Custom(models.Model):
             product_pricelist_items = self.env['product.pricelist.item'].search([('product_tmpl_id','=',record.id)])
             if product_pricelist_items and len(product_pricelist_items):
                 for data in product_pricelist_items:
-                    data.x_price_cost_difference = (data.fixed_price - record.standard_price)
+                    if data.compute_price == 'fixed':
+                        data.x_price_cost_difference = data.fixed_price - record.standard_price
+                    elif data.compute_price == 'percentage':
+                        data.x_price_cost_difference = (record.list_price - (data.percent_price * record.list_price / 100)) - record.standard_price
+                    elif data.compute_price == 'formula':
+                        data.x_price_cost_difference = (record.list_price - (data.price_discount * record.list_price / 100) + data.price_surcharge) - record.standard_price
                 record.x_list_price_sale = product_pricelist_items
             else:
                 record.x_list_price_sale = False
