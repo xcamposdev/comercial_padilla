0e8d1e1b7c4503e05ace12fc07801935c2038ac3
acastillo456 <acastillo@develoop.net>
2021-06-14 08:00:58 -0400

    Buscador nombre comercial

---
+++ custom_modules/res_partner_search_comercial/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contacto buscadar nombre comercial custom",
+    'version': "1",
+    'author': "Develoop Software",
+    'category': "Partner",
+    'summary': "Añade el filtro de busqueda de comercial",
+    'website': "https://www.develoop.net/",
+    'description': """
+        Añade el filtro de busqueda de comercial
+    """,
+    'depends': ['base'],
+    'data': [
+        'views/res_partner.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'demo': [], 
+    'installable': True,
+    'auto_install': False,
+}
