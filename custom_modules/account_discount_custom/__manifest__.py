a6dac5ad8c07ca6a820504bf503a846e4c46d163
acastillo456 <acastillo@develoop.net>
2021-06-15 09:24:44 -0400

    Purchase Discount Custom

---
+++ custom_modules/account_discount_custom/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'account discount custom',
+    'version': '1.0.0.0',
+    'author': 'Develoop Software S.A.',
+    'category': 'Develoop',
+    'website': 'https://www.develoop.net/',
+    'depends': ['base','account'],
+    'summary': 'Descuento en contabilidad',
+    'description': """
+        Descuento en contabilidad
+        """,
+    'data': [
+        'views/account_discount.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'demo': [],
+    'installable': True,
+}
