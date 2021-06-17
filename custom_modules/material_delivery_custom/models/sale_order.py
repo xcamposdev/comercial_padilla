4841cd8ebb5d1e875f39c66d1d26fda4a01be3a8
acastillo456 <acastillo@develoop.net>
2021-06-17 18:05:01 -0400

    Actualizacion entrega de material

--- custom_modules/material_delivery_custom/models/sale_order.py
+++ custom_modules/material_delivery_custom/models/sale_order.py
@@ -1,5 +1,6 @@
 
 import logging
+from operator import truediv
 #from server.odoo.fields import Float
 
 from odoo import api, fields, models, tools
@@ -18,7 +19,7 @@ class material_delivery_sale_order(models.Model):
         sale = super(material_delivery_sale_order, self).action_confirm()
 
         #-------------------GENERAR N Movimientos------------------------
-        location_default = self.env['ir.config_parameter'].sudo().get_param('x_md_default_location_cornella')
+        #location_default = self.env['ir.config_parameter'].sudo().get_param('x_md_default_location_cornella')
         warehouse_cornella = self.env['ir.config_parameter'].sudo().get_param('x_md_warehouse_cornella')
         operation_type_cornella = int(self.env['ir.config_parameter'].sudo().get_param('x_md_operation_type_cornella'))
         warehouse_cornella = warehouse_cornella.split()
@@ -27,29 +28,33 @@ class material_delivery_sale_order(models.Model):
             for line in self.order_line:
                 
                 product_qty = line.product_uom_qty
-                packaging = self.env['product.packaging'].search_read([('product_id','=',line.product_id.id)], fields=['id', 'qty','x_location','x_package'],  order='qty desc')
+                packaging = self.env['product.packaging'].search_read([('product_id','=',line.product_id.id)], fields=['id','name','qty','x_location','x_package'],  order='qty desc')
                 
                 while product_qty > 0:
                     if packaging and len(packaging):
                         
                         packing_qty = 0
-                        while product_qty > float(packaging[0]['qty']):
+                        while (product_qty > float(packaging[0]['qty'])) or (product_qty == 1):
                             packing_qty += float(packaging[0]['qty'])
                             product_qty -= float(packaging[0]['qty'])
 
                         if packing_qty != 0:
-                            #raise UserError('Mensaje de error')
-                            if packaging[0]['x_location'] ==False:
-                                packaging[0]['x_location'] = [int(location_default)]
+                            if packaging[0]['x_location'] == False:
+                                raise UserError('El empaquetado "%s" no tiene asignado una ubicación para el producto "%s"' % (packaging[0]['name'], line.product_id.name))
+                                #packaging[0]['x_location'] = [int(location_default)]
                             self.create_aditional_material_delivery(line, operation_type_cornella, packaging[0]['x_location'][0], packing_qty, packaging[0]['x_package'][0])
                         packaging.pop(0)
+
+                        if product_qty == 0:
+                            break
                     else:
-                        self.create_aditional_material_delivery(line, operation_type_cornella, int(location_default), product_qty)
-                        break
+                        raise UserError('No existe empaquetado de unidad para el producto "%s"' % line.product_id.name)
+                        # self.create_aditional_material_delivery(line, operation_type_cornella, int(location_default), product_qty)
+                        # break
         #----------------------------------------------------------------
         return sale
 
-    def create_aditional_material_delivery(self, line, operation_type_cornella, locationId, qty, packaging_id=False):
+    def create_aditional_material_delivery(self, line, operation_type_cornella, locationId, qty, packaging_id=False, execute_cron=False):
         move_search = self.env['stock.move'].search([
             ('product_id','=',line.move_ids[0].product_id.id),
             ('product_qty','=',line.move_ids[0].product_qty),
@@ -86,3 +91,12 @@ class material_delivery_sale_order(models.Model):
         }
         move = self.env['stock.move'].sudo().create(moves_values)
         move._action_confirm()
+
+        # if move.product_id.qty_available:
+        #     execute_cron = True
+        # return execute_cron
+        # product_qties = lines.mapped('product_id').with_context(to_date=scheduled_date, warehouse=warehouse).read([
+        #         'qty_available',
+        #         'free_qty',
+        #         'virtual_available',
+        #     ])
