e4be5aad10ed21c17379b1113f60c8347610d443
acastillo456 <acastillo@develoop.net>
2021-06-20 21:03:12 -0400

    Producto paquete

--- custom_modules/product_packaging_custom/models/stock_quant_custom.py
+++ custom_modules/product_packaging_custom/models/stock_quant_custom.py
@@ -15,11 +15,8 @@ class Stock_Quant_Custom(models.Model):
     def _compute_x_units_format(self):
         for record in self:
             if record.package_id:
-                packaging_id = self.env['product.packaging'].search([('id','=',record.package_id.id)], limit=1)
-                _logger.info(record.package_id.id)
-                _logger.info(packaging_id)
-                _logger.info(packaging_id.qty)
-                _logger.info(record.inventory_quantity)
+
+                packaging_id = self.env['product.packaging'].search([('x_package','=',record.package_id.id)], limit=1)
                 record.x_units_format = (packaging_id.qty or 0) / (record.inventory_quantity if record.inventory_quantity != 0 else 1)
             else:
                 record.x_units_format = 0
