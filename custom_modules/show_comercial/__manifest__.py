bc78346da7790cf2e3a1e94342537c74a7d06315
acastillo456 <acastillo@develoop.net>
2021-07-13 11:57:03 -0400

    Show comercial

---
+++ custom_modules/show_comercial/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "show_comercial_custom",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "Develoop",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['l10n_es_partner'],
+    'data': [
+        "views/res_partner_view.xml",
+    ],
+}
