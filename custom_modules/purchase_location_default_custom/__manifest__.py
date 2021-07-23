e7e116da7385f3dab72c5bac30f19e464d829f6a
samuelshock <samuelchq418@gmail.com>
2021-07-23 16:05:09 -0400

    Entrega: MOBILE: en las operaciones de tipo Recepcion, la ubicación por defecto destino debe ser CIN/WH

---
+++ custom_modules/purchase_location_default_custom/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Recepciones compras por defecto Custom",
+    "author": "Develoop",
+    "version": "13.0.0.1",
+    "category": "Purchase Management",
+    "website": "https://www.develoop.net/",
+    "depends": ["base", "purchase", "purchase_stock"],
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "images": ["static/description/icon.png"],
+}
