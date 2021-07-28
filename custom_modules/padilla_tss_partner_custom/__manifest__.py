3a53d94efa3268b53f94825aecd33c0e8a094403
samuelshock <samuelchq418@gmail.com>
2021-07-28 16:25:44 -0400

    Entrega de: Crear campo BOOL en ficha cliente PADILLA/TSS

---
+++ custom_modules/padilla_tss_partner_custom/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Padilla bool TSS clients',
+    'category': 'Develoop',
+    'version': '0.0',
+    'summary': """Agrega un campo booleando para clientes padilla PADILLA/TSS en la ficha cliente.""",
+    'description': """""",
+    'depends': ['base'],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'author': 'Develoop Software',
+    'images': ['static/description/icon.png'],
+    'maintainer': 'Develoop Software',
+    'website': 'https://www.develoop.net',
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
