ed406eaac424f8fe55c77a384a01d83acd67e222
samuelshock <samuelchq418@gmail.com>
2021-07-29 17:10:56 -0400

    Entrega: Al hacer ajuste de inventario desde APP error ubicacion padre

---
+++ custom_modules/barcode_inventory_custom/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Inventory barcode Custom',
+    'category': 'Sales',
+    'version': '0.0',
+    'summary': """Permite realizar ajustes de inventario sin verificar el padre.""",
+    'description': """""",
+    'depends': ["stock_barcode"],
+    'data': [
+        'views/assets.xml',
+    ],
+    'author': 'Develoop Software',
+    'images': ['static/description/icon.png'],
+    'maintainer': 'Develoop Software',
+    'website': 'https://www.develoop.net',
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
