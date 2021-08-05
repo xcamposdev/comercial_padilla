c611a879069788e206a0b51b91a117b8f20047e7
acastillo456 <acastillo@develoop.net>
2021-08-05 09:00:07 -0400

    Mostrar info a nivel de pedido MOBILE

---
+++ custom_modules/info_sp_so_mobile_custom/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Información de venta en stock picking Mobile Custom',
+    'version': '1.0.0.0',
+    'author': 'Develoop Software S.A.',
+    'category': 'stock_picking',
+    'website': 'https://www.develoop.net/',
+    'depends': ['base', 'web', 'stock', 'sale'],
+    'summary': 'información de venta en stock picking Mobile Custom',
+    'description': """
+        información de venta en stock picking Mobile Custom
+        """,
+    'data': [
+        'views/assets.xml'
+    ],
+    'qweb': [
+    ],
+    'images': ['static/description/icon.png'],
+    'demo': [],
+    'installable': True,
+}
