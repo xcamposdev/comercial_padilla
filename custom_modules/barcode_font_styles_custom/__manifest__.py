662301a6fca3f9227155f8067b7bbd06e494b0cd
samuelshock <samuelchq418@gmail.com>
2021-08-09 14:47:38 -0400

    Cambios en los estilos de la app codigo de barras

---
+++ custom_modules/barcode_font_styles_custom/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Stock Barcode styles Custom',
+    'category': 'Develoop',
+    'version': '0.0',
+    'summary': """Agrega estilos a las vistas de codigo de barras.""",
+    'description': """""",
+    'depends': ['base', 'barcodes', 'stock_barcode'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'author': 'Develoop Software',
+    'images': ['static/description/icon.png'],
+    'maintainer': 'Develoop Software',
+    'website': 'https://www.develoop.net',
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
