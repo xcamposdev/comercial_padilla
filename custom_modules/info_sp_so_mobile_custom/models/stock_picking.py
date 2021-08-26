68cd02e4447b8f5defede82a69c1768092c23443
acastillo456 <acastillo@develoop.net>
2021-08-26 13:07:27 -0400

    Update Weight

--- custom_modules/info_sp_so_mobile_custom/models/stock_picking.py
+++ custom_modules/info_sp_so_mobile_custom/models/stock_picking.py
@@ -12,7 +12,10 @@ class info_sp_so_mobile_stock_picking(models.Model):
     def get_origin_sale_kanban(self, origin):
         if origin:
             sale = self.env['sale.order'].search([('name','=',origin)], limit=1)
-            if sale:
-                return { 'partner_name': sale.partner_id.display_name, 'is_tss': sale.partner_id.x_is_tss, 'weight': 0, 'number_of_lines': len(sale.order_line) }
+            if sale and sale.order_line:
+                weight = 0
+                for line in sale.order_line:
+                    weight += line.product_id.weight * line.product_uom_qty
+                return { 'partner_name': sale.partner_id.display_name, 'is_tss': sale.partner_id.x_is_tss, 'weight': str(weight) + " " + str(line.product_id.weight_uom_name), 'number_of_lines': len(sale.order_line) }
         return False
-        
+		
